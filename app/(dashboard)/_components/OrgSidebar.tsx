7ce5baf1aa7a1774b1fbe12d483ac4201d4d8fcb
jbped <pedigojacob@gmail.com>
2024-02-14 11:14:49 -0700

    create initial orgsidebar component

---
+++ app/(dashboard)/_components/OrgSidebar.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function OrgSidebar() {
+  return (
+    <div className="hidden lg:flex flex-col space-y-6 w-52 pl-5 pt-5 bg-green-500">
+      OrgSidebar
+    </div>
+  )
+}
