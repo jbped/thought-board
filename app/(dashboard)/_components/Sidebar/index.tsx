56ff0bd48ed06764446e6861a4fe0f11f916e5eb
jbped <pedigojacob@gmail.com>
2024-02-14 11:14:26 -0700

    Create initial sidebar component

---
+++ app/(dashboard)/_components/Sidebar/index.tsx
@@ -0,0 +1,7 @@
+export default function Sidebar() {
+  return (
+    <aside className="fixed left-0 h-full w-16 flex flex-col gap-y-4 bg-primary text-white pt-5">
+      <p>Sidebar</p>
+    </aside>
+  );
+}
