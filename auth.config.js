942ea79e43dc29049b200b5c4abc31ff7de4f46f
jbped <pedigojacob@gmail.com>
2024-02-14 10:27:31 -0700

    Removed/moved files

--- auth.config.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
-  providers: [
-    {
-      domain: 'https://uncommon-marlin-56.clerk.accounts.dev',
-      applicationID: 'convex',
-    },
-  ],
-};
